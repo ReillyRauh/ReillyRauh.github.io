d95d5f5c22c840aa181cd0a55e0772228f8be268
Reilly Rauh <rrauh@Okeene.local>
2022-05-03 14:35:15 -0500

    2.0

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-let openBox = document.getElementById('open-box');
-let box = document.getElementById('options')
+const openBox = document.getElementById('open-box');
+const box = document.getElementById('options')
+const dateHolder = document.getElementById('date');
+const d = new Date()
+const y = d.getFullYear();
+var t = d.getDay() + 1;
+var m = d.getMonth() + 1;
 
 function revealOptions() {
     openBox.innerHTML = "Close Options..."
@@ -15,4 +20,20 @@ function closeOptions() {
     openBox.addEventListener('click', revealOptions);
 }
 
+function getTime() {
+    t = updateTime(t);
+    m = updateTime(m);
+
+    dateHolder.innerHTML = `Owned and Created by Reilly Rauh, Updated as of ${m}/${t}/${y}`;
+};
+
+function updateTime(y) {
+    if (y < 10) {
+        return '0' + y;
+    } else {
+        return y;
+    }
+}
+
 openBox.addEventListener('click', revealOptions);
+getTime();
--- websites/javascript-web/scripts/slider.js
+++ websites/javascript-web/scripts/slider.js
@@ -1,9 +1,9 @@
-const box = document.getElementById("box");
-const slider = document.getElementById("slider");
-const range = document.getElementById("range");
-
-range.innerHTML = slider.value;
+const output = document.getElementById('box');
+const slider = document.getElementById('range');
 
 slider.oninput = function() {
-    out
+    const convert = slider.value / 100;
+
+    output.style.opacity = convert;
+    document.getElementById('val').innerHTML = convert;
 }
